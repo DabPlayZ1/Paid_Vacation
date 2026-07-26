ba5198c5d634102390b9610e9f039697c354350a
Charmeleon565611 <Speed.o.char@gmail.com>
2026-07-26 14:11:30 +0500

    crosshair, collision shapes, models

---
+++ poster.gd
@@ -0,0 +1,5 @@
+extends StaticBody3D
+func Interact():
+	%Player.can_move = false
+	%"E interact".hide()
+	%TimePoster.show()
--- temp_vents.gd
+++ temp_vents.gd
@@ -4,5 +4,6 @@ extends StaticBody3D
 func Interact():
 	if %Player.has_screwdriver == true:
 		%temp_vents.hide()
+		%Player.global_position = Vector3(-20, 18, -8.65)
 		print("Escaped!")
 		queue_free()
